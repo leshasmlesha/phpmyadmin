6a0e0792a57a7db630e0c135cd2f8ace8e31a632
Alex Dev Sm <leshasmlesha@gmail.com>
2019-04-10 18:48:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 VER = 4_8_5
-VERin = 4.8.5
+VERIN = 4.8.5
 git clone https://github.com/phpmyadmin/phpmyadmin.git source
 cd source
 git checkout RELEASE_$VER
@@ -8,8 +8,8 @@ composer update --no-dev
 scripts/update-po
 rm -rf *
 git checkout .
-scripts/create-release.sh $VERin release
-cp release/phpMyAdmin-$VERin-source.tar.xz ../phpmyadmin_$VERin.orig.tar.xz
+scripts/create-release.sh $VERIN release
+cp release/phpMyAdmin-$VERIN-source.tar.xz ../phpmyadmin_$VERIN.orig.tar.xz
 cd ..
 rm -rf source
 curl -O http://archive.ubuntu.com/ubuntu/pool/universe/p/phpmyadmin/phpmyadmin_4.6.6-5.debian.tar.xz
@@ -17,7 +17,7 @@ mkdir phpmyadmin
 tar xf phpmyadmin_4.6.6-5.debian.tar.xz -C phpmyadmin
 rm phpmyadmin_4.6.6-5.debian.tar.xz
 #curl https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-source.tar.xz -o phpmyadmin_4.8.5.orig.tar.xz
-tar xf phpmyadmin_$VERin.orig.tar.xz -C phpmyadmin --strip-components=1
+tar xf phpmyadmin_$VERIN.orig.tar.xz -C phpmyadmin --strip-components=1
 rm phpmyadmin/debian/patches/Truncate-only-long-passwords.patch
 cd phpmyadmin/debian/
 curl -O https://leshasmlesha.github.io/phpmyadmin/changed/install
